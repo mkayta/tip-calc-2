63dcb4fab94cf023fbe208b2b08dd8929de496d2
Eng-Mohamed <mkayta2023@gmail.com>
2024-09-11 20:14:22 +0300

    I done this challenge

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+function calcTip(bill){
+    let Tip;
+if(bill>=50 && bill<=300){
+    Tip=bill*15/100
+}else{
+    Tip=bill*20/100
+}
+const totalvalue=bill+Tip
+console.log(`the bill was ${bill}, the Tip was ${Tip}, and the total value is ${totalvalue}`)
+ return Tip
+}
+  
+calcTip(800)
